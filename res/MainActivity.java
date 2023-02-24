002e1611bce44c0f94adfab1305ced06ef3a471f
iretro <100819408+beanz0110@users.noreply.github.com>
2023-02-23 23:40:37 -0500

    Create MainActivity.java

---
+++ res/MainActivity.java
@@ -0,0 +1,53 @@
+package com.zybooks.pizzaparty;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+   public final static int SLICES_PER_PIZZA = 8;
+
+   private EditText mNumAttendEditText;
+   private TextView mNumPizzasTextView;
+   private RadioGroup mHowHungryRadioGroup;
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_main);
+
+     
+       mNumAttendEditText = findViewById(R.id.num_attend_edit_text);
+       mNumPizzasTextView = findViewById(R.id.num_pizzas_text_view);
+       mHowHungryRadioGroup = findViewById(R.id.hungry_radio_group);
+   }
+
+   public void calculateClick(View view) {
+
+      
+       String numAttendStr = mNumAttendEditText.getText().toString();
+
+       int numAttend = Integer.parseInt(numAttendStr);
+      
+       
+       int slicesPerPerson = 0;
+       int checkedId = mHowHungryRadioGroup.getCheckedRadioButtonId();
+       if (checkedId == R.id.light_radio_button) {
+           slicesPerPerson = 2;
+       }
+       else if (checkedId == R.id.medium_radio_button) {
+           slicesPerPerson = 3;
+       }
+       else if (checkedId == R.id.ravenous_radio_button) {
+           slicesPerPerson = 4;
+       }
+
+       // Calculate and show the number of pizzas needed
+       int totalPizzas = (int) Math.ceil(numAttend * slicesPerPerson / (double) SLICES_PER_PIZZA);
+       mNumPizzasTextView.setText("Total pizzas: " + totalPizzas);
+   }
+}
